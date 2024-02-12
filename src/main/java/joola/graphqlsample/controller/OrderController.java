0ab42076af22b3637012e97a4267cbdbeadff03c
fatemehjj <joolafatemeh652@gmail.com>
2024-02-12 22:31:41 +0330

    add order controller and 2 query mapping functions

---
+++ src/main/java/joola/graphqlsample/controller/OrderController.java
@@ -0,0 +1,26 @@
+package joola.graphqlsample.controller;
+
+import joola.graphqlsample.model.Order;
+import joola.graphqlsample.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+    @Autowired
+    OrderRepository orderRepository;
+    @QueryMapping
+    public List<Order> allOrders(){
+        return orderRepository.findAll();
+    }
+    @QueryMapping
+    public List<Order> findByOwnerName(@Argument String owner){
+        return orderRepository.findByOwnerName(owner);
+    }
+}
+
