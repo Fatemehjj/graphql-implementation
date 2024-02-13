4a839063cbc1ea8b71f74e0ca6edfbdd948b3c62
fatemehjj <joolafatemeh652@gmail.com>
2024-02-13 20:31:25 +0330

    remove some unused packages

--- src/main/java/joola/graphqlsample/controller/OrderController.java
+++ src/main/java/joola/graphqlsample/controller/OrderController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
